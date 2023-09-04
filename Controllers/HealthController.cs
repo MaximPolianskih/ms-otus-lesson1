8c13f6e208f91ea6254088f10913ec1d65a548d8
Maxim <Crus_nic@hotmail.com>
2023-09-04 21:12:40 +0300

    Add project files.

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public IActionResult Check()
+        {
+            _logger.LogInformation($"Метод {nameof(Check)} был вызван");
+
+            return Ok();
+        }
+    }
+}
